f56f238591562ab387b9f5dbc081093e297b0b67
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-02-12 16:10:17 -0500

    Avances

--- Contest 2/Contest 2/ViewController.swift
+++ Contest 2/Contest 2/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var hasFilledField: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    
+    @IBAction func submit(_ sender: Any) {
+        if(emailField.text != "") {
+            performSegue(withIdentifier: "Submit", sender: nil)
+            self.emailField.layer.borderColor = nil
+            self.emailField.layer.borderWidth = 0
+            self.emailField.text = ""
+        } else {
+            let myColor = UIColor.red
+            
+            
+            
+            UIView.animate(withDuration: 1, animations: {
+                self.emailField.layer.borderColor = myColor.cgColor
+                self.emailField.layer.borderWidth = 1.0
+            })
+        }
+    }
 }
 
