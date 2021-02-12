99f152337e3b9899c4c96a57b49ff8d36eb82879
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-02-12 15:11:34 -0500

    Segundo intent

---
+++ Contest 2/Contest 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contest 2
+//
+//  Created by user183079 on 2/12/21.
+//  Copyright © 2021 user183079. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
