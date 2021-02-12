30aad8fc5d55bfb1604545632631e8ffa6024a0b
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2021-02-12 14:15:19 -0500

    Proyecto contest

---
+++ Contest/Contest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contest
+//
+//  Created by user183079 on 2/12/21.
+//  Copyright © 2021 user183079. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Lab-Animations.playground/Contents.swift
+++ Lab-Animations.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+//Inicializar el liveview
+let liveViewFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
+let liveView = UIView(frame: liveViewFrame)
+liveView.backgroundColor = .white
+
+PlaygroundPage.current.liveView = liveView
+
+// Contenido
+let smallFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+let square = UIView(frame: smallFrame)
+square.backgroundColor = .purple
+
+liveView.addSubview(square)
+
+UIView.animate(withDuration: 2.0) {
+    square.backgroundColor = .orange
+    let scaleTransform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+    let rotateTransform = CGAffineTransform(rotationAngle: .pi)
+    let translateTransform = CGAffineTransform(translationX: 200, y: 200)
+    let comboTransform = scaleTransform.concatenating(rotateTransform).concatenating(translateTransform);
+    
+    square.transform = comboTransform
+}
